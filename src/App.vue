33ff74c930f404ec3198ae13e2dcf432055d235f
Steveh77 <stederosa76@gmail.com>
2022-07-29 14:12:29 +0200

    Milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MySearchBar @search="searchMovies" />
+
+    <div>
+      <ul v-for="movie in movies" :key="movie.id">
+        <li>{{ movie.title }}</li>
+        <li>{{ movie.original_title }}</li>
+        <li>{{ movie.vote_average }}</li>
+        <li>{{ movie.original_language }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MySearchBar from "./components/MySearchBar.vue";
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MySearchBar
+  },
+  data() {
+    return {
+      movies: [],
+      api: {
+        baseUri: "https://api.themoviedb.org/3",
+        key: "ee60758c3a831bd3e6eef24c05e5e7b6",
+        language: "it-IT"
+
+      }
+    }
+  },
+  methods: {
+    searchMovies(inputText) {
+      // chiamare API
+      axios.get(`${this.api.baseUri}/search/movie?api_key=${this.api.key}&query=${inputText}&language=${this.language}`).then((res) => {
+        this.movies = res.data.results
+      })
+
+    }
+  },
 }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MySearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="text" placeholder="cerca.." v-model="inputText" @keyup.enter="$emit('search', inputText)">
+        <button @click="$emit('search', inputText)">Cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputText: ""
+        }
+    },
+}
+</script>
+
+<style>
+</style>
