6a30e9540d9d819a0f5678d87f7a2e5a0c5c7d0f
Steveh77 <stederosa76@gmail.com>
2022-07-29 17:53:54 +0200

    Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <div id="app">
-    <MySearchBar @search="searchMovies" />
+    <MySearchBar @search="searchData" />
+    <div class="container">
+      <div>
+        <h3>Film</h3>
+        <TheCard v-for="movie in movies" :key="movie.id" :production="movie" />
+      </div>
+      <div>
+        <h3>Serie Tv</h3>
+        <TheCard v-for="serie in series" :key="serie.id" :production="serie" />
 
-    <div>
-      <ul v-for="movie in movies" :key="movie.id">
-        <li>{{ movie.title }}</li>
-        <li>{{ movie.original_title }}</li>
-        <li>{{ movie.vote_average }}</li>
-        <li>{{ movie.original_language }}</li>
-      </ul>
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
 import MySearchBar from "./components/MySearchBar.vue";
 import axios from "axios"
+import TheCard from "./components/TheCard.vue"
 
 export default {
   name: 'App',
   components: {
-    MySearchBar
+    MySearchBar,
+    TheCard,
   },
   data() {
     return {
       movies: [],
+      series: [],
       api: {
         baseUri: "https://api.themoviedb.org/3",
         key: "ee60758c3a831bd3e6eef24c05e5e7b6",
@@ -33,18 +39,33 @@ export default {
       }
     }
   },
+  computed: {
+    itHadFlag() {
+      const flags = ['it', 'en'];
+      return flags.includes(this.movies.original_language)
+    }
+  },
   methods: {
-    searchMovies(inputText) {
+    searchData(inputText) {
+      // controllo
+      if (!inputText) this.movies = this.series = [];
+
       // chiamare API
       axios.get(`${this.api.baseUri}/search/movie?api_key=${this.api.key}&query=${inputText}&language=${this.language}`).then((res) => {
         this.movies = res.data.results
-      })
-
-    }
+      });
+      axios.get(`${this.api.baseUri}/search/tv?api_key=${this.api.key}&query=${inputText}&language=${this.language}`).then((res) => {
+        this.series = res.data.results
+      });
+    },
   },
 }
 
 </script>
 
 <style lang="scss">
+.container {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,35 @@
+@<template>
+    <ul>
+        <li>{{ production.title || production.name }}</li>
+        <li>{{ production.original_title || production.original_name }}</li>
+        <li>{{ production.vote_average }}</li>
+        <li>
+            <img v-if="itHadFlags" :src="flagImg" :alt="production.original_language">
+            <span v-else>{{ production.original_language }}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        production: Object
+    },
+    computed: {
+        itHadFlags() {
+            const flags = ['it', 'en']
+            return flags.includes(this.production.original_language)
+        },
+        flagImg() {
+            return require(`../assets/flags/${this.production.original_language}.png`)
+        }
+    },
+}
+</script>
+
+<style>
+img {
+    height: 50px;
+    width: 70px;
+}
+</style>
