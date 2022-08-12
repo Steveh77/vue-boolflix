b91ab79017cefb1c41d66f20479880d23a0d4f58
Steveh77 <stederosa76@gmail.com>
2022-08-12 17:55:20 +0200

    Milestone 3

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,10 +1,13 @@
 @<template>
     <ul>
+        <li>
+            <img :src='getImg'>
+        </li>
         <li>{{ production.title || production.name }}</li>
         <li>{{ production.original_title || production.original_name }}</li>
-        <li>{{ production.vote_average }}</li>
+        <li>{{ getstars }}</li>
         <li>
-            <img v-if="itHadFlags" :src="flagImg" :alt="production.original_language">
+            <img v-if="itHadFlags" :src="flagImg" :alt="production.original_language" class="flag">
             <span v-else>{{ production.original_language }}</span>
         </li>
     </ul>
@@ -16,20 +19,44 @@ export default {
         production: Object
     },
     computed: {
+        getImg() {
+            return `https://image.tmdb.org/t/p/w342/${this.production.poster_path}`
+        },
         itHadFlags() {
             const flags = ['it', 'en']
             return flags.includes(this.production.original_language)
         },
         flagImg() {
             return require(`../assets/flags/${this.production.original_language}.png`)
+        },
+        getstars() {
+            let valutation = Math.ceil(this.production.vote_average / 2)
+            let starValutation = ""
+            if (valutation === 0) {
+                starValutation = "Non valutato"
+            } else {
+                for (let i = 0; i < valutation; i++) {
+                    starValutation += '⭐';
+                }
+            }
+            return starValutation
         }
     },
 }
 </script>
 
 <style>
-img {
+.flag {
     height: 50px;
     width: 70px;
+}
+
+ul {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    list-style-type: none;
+    padding: 0;
 }
 </style>
